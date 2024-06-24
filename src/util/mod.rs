4653dd9f667c923dc3be19107f77c77897ab9266
LeoLuxo <leo@gamboa.eu>
2024-06-24 17:10:38 +0200

    Move shader to pbr_tracer for the time being

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,7 +2,7 @@ reexport_feature_module!(angle);
 reexport_feature_module!(camera_3d);
 reexport_feature_module!(convert);
 reexport_feature_module!(path);
-reexport_feature_module!(shader);
+// reexport_feature_module!(shader);
 reexport_feature_module!(speed);
 reexport_feature_module!(texture);
 reexport_feature_module!(tuples);
